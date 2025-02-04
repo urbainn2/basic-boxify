0b6cb49752fa3d0bebfc8ea54fbd8fa163daaeb5
Kristina Creek <144278926+KCreek1@users.noreply.github.com>
2025-02-04 11:35:11 -0700

    set default to white instead of purple

--- lib/theme/boxify_theme.dart
+++ lib/theme/boxify_theme.dart
@@ -3,13 +3,38 @@ import 'package:boxify/app_core.dart';
 
 class BoxifyTheme {
   static ThemeData buildTheme() {
-    return ThemeData(
-      dialogTheme: DialogTheme(
-        elevation: 10,
-        shape: Core.appUI.shapeBorder,
-        backgroundColor: Core.appColor.background,
-        surfaceTintColor: Core.appColor.background,
-        contentTextStyle: Core.appStyle.small,
+    return ThemeData.dark().copyWith(      
+      scaffoldBackgroundColor: Core.appColor.scaffoldBackgroundColor,
+      primaryColor: const Color.fromRGBO(30, 30, 30, 1),
+
+      // Text theme with white text across all body text styles
+      textTheme: ThemeData.dark().textTheme.copyWith(
+        bodyLarge: const TextStyle(color: Colors.white),
+        bodyMedium: const TextStyle(color: Colors.white),
+        bodySmall: const TextStyle(color: Colors.white),
+      ),
+      
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white, // White text on buttons
+        ),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white, // White text on buttons
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: Colors.white, // White text on buttons
+        ),
+      ),
+
+      iconTheme: const IconThemeData(color: Colors.white),  // White icons
+
+      appBarTheme: AppBarTheme(
+        backgroundColor: const Color.fromRGBO(30, 30, 30, 1), // AppBar background color
+        iconTheme: const IconThemeData(color: Colors.white), // White icons in AppBar
       ),
     );
   }
