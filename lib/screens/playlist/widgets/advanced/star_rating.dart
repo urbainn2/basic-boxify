e311224c3b09f01c987ba73223194c39715d10ba
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2025-02-25 17:07:16 -0800

    comment

--- lib/screens/playlist/widgets/advanced/star_rating.dart
+++ lib/screens/playlist/widgets/advanced/star_rating.dart
@@ -28,13 +28,13 @@ class StarRating extends StatelessWidget {
       builder: (context, state) {
         final isAnonymous = context.read<UserBloc>().state.user.id == '';
 
-    // Get the rating from the user's ratings map/list
-    final ratingFromUserBloc = state.ratings
-        .firstWhere(
-          (r) => r.trackUuid == track.uuid,
-          orElse: () => Rating(trackUuid: track.uuid!, value: 0),
-        )
-        .value;
+        // Get the rating from the user's ratings map/list
+        final ratingFromUserBloc = state.ratings
+            .firstWhere(
+              (r) => r.trackUuid == track.uuid,
+              orElse: () => Rating(trackUuid: track.uuid!, value: 0),
+            )
+            .value;
         return RatingBar.builder(
           // initialRating: track.userRating ?? 0.0, // https://github.com/riverscuomo/boxify/issues/108
           initialRating: ratingFromUserBloc ?? 0.0,
@@ -51,6 +51,7 @@ class StarRating extends StatelessWidget {
               context.read<AuthBloc>().add(AuthLogoutRequested());
               GoRouter.of(context).push('/login');
             } else if (v > 0) {
+              // If we wanted to keep using the track.userRating, we would need to update it here, or somehow have this cascade down to the track.userRating
               context
                   .read<UserBloc>()
                   .add(UpdateRating(trackId: track.uuid!, value: v));
